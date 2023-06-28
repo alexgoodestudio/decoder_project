a560ce6086ca5a58f6ebd10332cd90e53a727c55
Alex Goode <alexgoodestudio@gmail.com>
2023-06-27 22:48:43 -0400

    caesar-test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,23 @@
-// Write your tests here!
+const expect = require ("chai").expect
+const caesar = require("../src/caesar")
+
+describe("caesar", () =>{
+    it("should return the an encoded word", () =>{
+        const input = [
+            caesar("thinkful", 3) 
+        ]
+    const expected = 'wklqnixo'
+    const actual = caesar(input, shift, encode = true)
+    expect(actual).to.equal(expected)
+    })
+})
+describe("caesar", () =>{
+    it("should return the an decoded word", () =>{
+        const input = [
+            caesar("wklqnixo", 3, false)
+        ]
+    const expected = 'thinkful'
+    const actual = caesar(input, shift, encode = false)
+    expect(actual).to.equal(expected)
+    })
+})
