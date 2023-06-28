e086f927c9898c699a9145b6479c47607e3c6865
Alex Goode <alexgoodestudio@gmail.com>
2023-06-28 14:27:27 -0400

    logs

--- src/polybius.js
+++ src/polybius.js
@@ -35,18 +35,33 @@ const letters =
   }
 
   function polybius(input, encode = true) {
-     //initialize an empty string for decoded value
-    //split input into individual elements(split returns a new array)
-    //set that value = value.toLowercase
-    //with that value, see if letters include input
-    //return letters.indexOf element
-    // spaces should be maintained throughout 
-    // set value to decoded
+    
+//invalid
+    //**if** (input % 3)return false 
+    if(input % 3){return false} 
+//encode
+    
+    if(encode === true){
+    //need to split input into separate array elements 
+    let inputArray = input.toLowerCase().split("")
+    console.log(inputArray)
+    return inputArray
+    
+    //check if letters object includes input array
+    // message = letters.includes(input)
+    //let decode = letters.indexOf(input)
+    //return decode
+    }
 
-    // if input includes letter
-    // decoded = value.join
-    // output should still be a string 
+//---------------------------------
+//decode
 
+    //**else** split input into individual elements(split returns a new array) ex [T,h,i,n,k,f,u,l]
+    //set that inputArray = inputArray.toLowercase ex [t,h,i,n,k,f,u,l]
+    //for in loop | iterates through letters object 
+    //if letters.include(inputArray)
+    //encoded = letters.indexOf(element)
+    //return encoded
   }
 
   return {
@@ -55,3 +70,26 @@ const letters =
 })();
 
 module.exports = { polybius: polybiusModule.polybius };
+
+/*
+
+
+polybius()
+
+input refers to the inputted text to be encoded or decoded.
+encode refers to whether you should encode or decode the message. By default it is set to true.
+When building the function, keep the following constraints and rules in mind:
+
+You are welcome to assume that no additional symbols will be included as part of the input. Only spaces and letters will be included.
+When encoding, your output should still be a string.
+When decoding, the number of characters in the string excluding spaces should be even. Otherwise, return false.
+Spaces should be maintained throughout.
+Capital letters can be ignored.
+The letters I and J share a space. When encoding, both letters can be converted to 42, but when decoding, both letters should somehow be shown.
+Examples
+polybius("thinkful"); //> "4432423352125413"
+polybius("Hello world"); //> '3251131343 2543241341'
+
+polybius("3251131343 2543241341", false); //> "hello world"
+polybius("4432423352125413", false); //> "th(i/j)nkful
+polybius("44324233521254134", false); //> false */
