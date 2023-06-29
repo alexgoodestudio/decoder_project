56d1b555edb2696e8cb2dceb7c1bd9fe36491b5d
Alex Goode <alexgoodestudio@gmail.com>
2023-06-29 19:01:05 -0400

    halfcheckpointsub

--- src/substitution.js
+++ src/substitution.js
@@ -6,8 +6,43 @@
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
-  function substitution(input, alphabet, encode = true) {
+  function substitution(input, alphabet = "", encode = true) {
     // your solution code here
+    const standardAlphabet = [
+      'a', 'b', 'c', 'd', 'e', 'f',
+      'g', 'h', 'i', 'j', 'k', 'l',
+      'm', 'n', 'o', 'p', 'q', 'r',
+      's', 't', 'u', 'v', 'w', 'x',
+      'y', 'z'
+    ];
+// alphabet = bsdas
+//a  b  c  d  e  
+//b  s  d  a  s  
+const splitstring = alphabet.split("")
+const inputArr = input.split("")
+//ENCODE =true
+//if alphabet is longer than 26 characters return false, must be a string
+if(encode === true && alphabet.length === 26){
+  const output = inputArr.map((character) =>{
+    if(character === " "){
+      return " "
+    }
+    let standAlpInd = standardAlphabet.indexOf(character)
+    return splitstring[standAlpInd]
+    })
+    console.log(output, "******")
+    return output.join("")
+}
+
+//DECODE =false
+//if alphabet is longer than 26 characters return false
+//
+if(encode === false && alphabet.length === 26){
+
+}
+
+
+
   }
 
   return {
@@ -16,3 +51,38 @@ const substitutionModule = (function () {
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
+/*
+The substitution cipher requires a standard alphabet and a substitution alphabet. Letters from the standard alphabet will be transposed to the substitution alphabet. This cipher requires that the recipient have the substitution alphabet, otherwise it will be difficult for them to decode the message.
+
+For example, in the image above, the word HELLO would be translated as follows:
+
+H becomes R.
+E becomes M.
+L becomes W.
+O becomes L.
+This would result in the code RMWWL. To decrypt this code, you would simply take the result and transpose back from the substitution alphabet to the standard alphabet.
+
+substitution()
+The substitution() function in the src/substitution.js file has three parameters:
+
+input refers to the inputted text to be encoded or decoded.
+alphabet refers to substitution alphabet.
+encode refers to whether you should encode or decode the message. By default it is set to true.
+When building the function, keep the following constraints and rules in mind:
+
+The input could include spaces and letters as well as special characters such as #, $, *, etc.
+Spaces should be maintained throughout.
+Capital letters can be ignored.
+The alphabet parameter must be a string of exactly 26 characters, which could include special characters such as #, $, *, etc. Otherwise, it should return false.
+All the characters in the alphabet parameter must be unique. Otherwise, it should return false.
+Examples
+substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev"); //> 'jrufscpw'
+substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev"); //> 'elp xhm xf mbymwwmfj dne'
+substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false); //> 'thinkful'
+
+substitution("message", "$wae&zrdxtfcygvuhbijnokmpl"); //> "y&ii$r&"
+substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false); //> "message"
+
+substitution("thinkful", "short"); //> false
+substitution("thinkful", "abcabcabcabcabcabcabcabcyz"); //> false
+*/
